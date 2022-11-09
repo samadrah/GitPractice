339a2c20a60c633362dbb77cb8ba9c05d17e3967
samadrah <117692067+samadrah@users.noreply.github.com>
2022-11-08 20:12:03 -0500

    Update TestClass.java

--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -5,5 +5,11 @@ public class TestClass {
 	public void testClass() {
 		System.out.println("this is from local repo");
 	}
+	
+	
+	
+	public void testClass2() {
+		System.out.println("this is from local repo");
+	}
 
 }
